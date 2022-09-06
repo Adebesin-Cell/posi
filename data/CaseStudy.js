3ed6c8b8d4361881044a0960db33213ed4b64d59
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-06 23:09:19 +0100

    Updating UI

--- data/CaseStudy.js
+++ data/CaseStudy.js
@@ -18,19 +18,19 @@ export const CaseStudies = [
     description:
       'A project designed to make shopping easier for its users. Of course, there are lots of online shopping platforms, but it was thought that the need for a shopping platform that would put the needs of its users upfront is needed.',
   },
-  {
-    id: 2,
-    title: 'Product Designer',
-    name: 'Slay & Co',
-    image: caseStudyOne,
-    tag: 'Fashion',
-    tagBackground: '#FBFF34',
-    tagColor: '#141414',
-    liveLink: '',
-    caseStudyLink: '/case-study/2',
-    description:
-      'A fashion brand website landing page designed by using the design thinking process  to solve the problem of confusion from users.',
-  },
+  // {
+  //   id: 2,
+  //   title: 'Product Designer',
+  //   name: 'Slay & Co',
+  //   image: caseStudyOne,
+  //   tag: 'Fashion',
+  //   tagBackground: '#FBFF34',
+  //   tagColor: '#141414',
+  //   liveLink: '',
+  //   caseStudyLink: '/case-study/2',
+  //   description:
+  //     'A fashion brand website landing page designed by using the design thinking process  to solve the problem of confusion from users.',
+  // },
   {
     id: 3,
     title: 'Product Designer',
