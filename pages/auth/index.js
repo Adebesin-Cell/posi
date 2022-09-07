24fce8a2c65ef62d95e08dfc2be6375c217ce03d
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-07 23:33:12 +0100

    Updating UI

---
+++ pages/auth/index.js
@@ -0,0 +1,5 @@
+const AuthPage = function () {
+  return <div className='lol'>Hey, Login here</div>;
+};
+
+export default AuthPage;
---
+++ pages/auth/register.js
@@ -0,0 +1,5 @@
+const Register = function () {
+  return <div className='lol'>Hey, Register here</div>;
+};
+
+export default Register;
