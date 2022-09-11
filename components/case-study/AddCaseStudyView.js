7ec78ce56681e2895e03be6ff03a59f5154c72dd
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-11 07:00:10 +0100

    Updating UI

--- components/case-study/AddCaseStudyView.js
+++ components/case-study/AddCaseStudyView.js
@@ -4,28 +4,69 @@ import Tiptap from '../text-editor/Editor';
 import styles from './AddCaseStudyView.module.scss';
 import Input from '../ui/input/Input';
 import { PlusIcon } from '../../utils/icons/Icons';
-import { useEffect, useState } from 'react';
+import useFileUpload from '../../hooks/use-file-upload';
+import { useState } from 'react';
 
 const AddCaseStudyView = function () {
-  const [caseStudyCoverImage, setcaseStudyCoverImage] = useState();
-  const [imageUrl, setImageUrl] = useState();
+  const [colors, setColors] = useState([]);
 
-  const caseStudyCoverImageUpload = function (e) {
-    setcaseStudyCoverImage((prevImage) => {
-      return e.target.files[0];
-    });
-  };
+  const {
+    uploadFuntionHandler: uploadCoverImage,
+    images: CoverImages,
+    imageUrls: CoverImageURLS,
+  } = useFileUpload();
+
+  const {
+    uploadFuntionHandler: uploadEmpathyImage,
+    images: EmpathyImages,
+    imageUrls: EmpathyImageURLS,
+  } = useFileUpload();
+
+  const {
+    uploadFuntionHandler: uploadUserPersonas,
+    images: UserPersonas,
+    imageUrls: UserPersonasURLS,
+  } = useFileUpload();
+
+  const {
+    uploadFuntionHandler: uploadUserflow,
+    images: userflows,
+    imageUrls: userflowURLS,
+  } = useFileUpload();
+
+  const {
+    uploadFuntionHandler: uploadIdeationImage,
+    images: ideationImages,
+    imageUrls: ideationImagesURL,
+  } = useFileUpload();
 
   const getEditorContent = function (html) {
     console.log(html);
   };
 
-  useEffect(() => {
-    if (!caseStudyCoverImage) return;
-    const CaseStudyImageURL = URL.createObjectURL(caseStudyCoverImage);
-    setImageUrl(CaseStudyImageURL);
-    console.log(CaseStudyImageURL);
-  }, [caseStudyCoverImage]);
+  const generateNewColorHandler = function () {
+    setColors((prevColors) => [
+      ...prevColors,
+      { id: Date.now().toString(), colorCode: '#ffffff' },
+    ]);
+  };
+
+  const changeColorHandler = function (e, id) {
+    setColors((prevColors) => {
+      const currentColorBox = prevColors.find((color) => color.id === id);
+      currentColorBox.colorCode = e.target.innerHTML;
+      return [...prevColors];
+    });
+  };
+
+  const deleteColor = function (id) {
+    setColors((prevColors) => {
+      const updatedColors = prevColors.filter((color) => color.id !== id);
+      return [...updatedColors];
+    });
+  };
+
+  const CONVERT_IMAGE_SIZE_MB = 1000000;
 
   return (
     <div className={styles.view}>
@@ -61,36 +102,36 @@ const AddCaseStudyView = function () {
                 name='case-study-image'
                 className={styles.view__file}
                 id='case-study-cover-image'
-                onChange={caseStudyCoverImageUpload}
+                onChange={uploadCoverImage}
                 accept='image/*'
               />
               <div className={styles.upload}>
                 <label htmlFor='case-study-cover-image'>
                   <div className={styles.upload__card}>
-                    {!caseStudyCoverImage && (
+                    {!CoverImages[0] && (
                       <div className={styles.upload__icon}>
                         <PlusIcon />
                       </div>
                     )}
-                    {caseStudyCoverImage && (
+                    {CoverImages[0] && (
                       <img
-                        src={imageUrl}
-                        alt={caseStudyCoverImage.name}
+                        src={CoverImageURLS[0]}
+                        alt={CoverImages[0].name}
                         className={styles.upload__image}
                       />
                     )}
                     <div className={styles.upload__details}>
                       <h1 className={styles.upload__name}>
-                        {caseStudyCoverImage && caseStudyCoverImage.name}
-                        {!caseStudyCoverImage && 'Case Study Image'}
+                        {CoverImages[0] && CoverImages[0].name}
+                        {!CoverImages[0] && 'Case Study Image'}
                       </h1>
                       <p className={styles.upload__size}>
-                        {caseStudyCoverImage &&
-                          `${(caseStudyCoverImage.size / 1000000).toFixed(
-                            2
-                          )}mb`}
+                        {CoverImages[0] &&
+                          `${(
+                            CoverImages[0].size / CONVERT_IMAGE_SIZE_MB
+                          ).toFixed(2)}mb`}
 
-                        {!caseStudyCoverImage && 'Size'}
+                        {!CoverImages[0] && 'Size'}
                       </p>
                     </div>
                   </div>
@@ -111,12 +152,6 @@ const AddCaseStudyView = function () {
             ></textarea>
           </div>
           <div className={styles.view__group}>
-            <label htmlFor='case-study-about' className={styles.view__title}>
-              About Project
-            </label>
-            <Tiptap getEditorContent={getEditorContent} />
-          </div>
-          <div className={styles.view__group}>
             <div className={styles.view__wrapper}>
               <label htmlFor='case-study-type' className={styles.view__label}>
                 Project Type
@@ -129,6 +164,12 @@ const AddCaseStudyView = function () {
                 id='case-study-type'
               />
             </div>
+          </div>
+          <div className={styles.view__group}>
+            <label htmlFor='case-study-about' className={styles.view__title}>
+              About Project
+            </label>
+            <Tiptap getEditorContent={getEditorContent} />
           </div>
           <div className={styles.view__group}>
             <div className={styles.view__wrapper}>
@@ -147,22 +188,278 @@ const AddCaseStudyView = function () {
               />
             </div>
           </div>
+          <div className={styles.view__group}>
+            <div className={styles.view__wrapper}>
+              <label htmlFor='case-study-tools' className={styles.view__label}>
+                Tools
+              </label>
+              <Input
+                className={styles.view__input}
+                placeholder='Enter Tools'
+                type='text'
+                name='case-study-tools'
+                id='case-study-tools'
+              />
+            </div>
+          </div>
+          <div className={styles.view__group}>
+            <div className={styles.view__wrapper}>
+              <label htmlFor='case-study-type' className={styles.view__label}>
+                Roles
+              </label>
+              <Input
+                className={styles.view__input}
+                placeholder='Enter Roles'
+                type='text'
+                name='case-study-roles'
+                id='case-study-roles'
+              />
+            </div>
+          </div>
+          <div className={styles.view__group}>
+            <div className={styles.view__card}>
+              <Input
+                type='file'
+                name='case-study-empathy-image'
+                className={styles.view__file}
+                id='case-study-empathy-image'
+                onChange={uploadEmpathyImage}
+                accept='image/*'
+              />
+              <div className={styles.upload}>
+                <label htmlFor='case-study-empathy-image'>
+                  <div className={styles.upload__card}>
+                    {!EmpathyImages[0] && (
+                      <div className={styles.upload__icon}>
+                        <PlusIcon />
+                      </div>
+                    )}
+                    {EmpathyImages[0] && (
+                      <img
+                        src={EmpathyImageURLS[0]}
+                        alt={EmpathyImages[0].name}
+                        className={styles.upload__image}
+                      />
+                    )}
+                    <div className={styles.upload__details}>
+                      <h1 className={styles.upload__name}>
+                        {EmpathyImages[0] && EmpathyImages[0].name}
+                        {!EmpathyImages[0] && 'Add Image'}
+                      </h1>
+                      <p className={styles.upload__size}>
+                        {EmpathyImages[0] &&
+                          `${(
+                            EmpathyImages[0].size / CONVERT_IMAGE_SIZE_MB
+                          ).toFixed(2)}mb`}
+
+                        {!EmpathyImages[0] && 'Size'}
+                      </p>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className={styles.view__group}>
+            <label htmlFor='case-study-empathy' className={styles.view__title}>
+              Emphaty
+            </label>
+            <Tiptap getEditorContent={getEditorContent} />
+          </div>
+          <div className={styles.view__group}>
+            <label
+              htmlFor='case-study-user-persona'
+              className={styles.view__title}
+            >
+              User Persona
+            </label>
+            <Input
+              type='file'
+              name='case-study-image'
+              className={styles.view__file}
+              id='case-study-user-persona'
+              onChange={uploadUserPersonas}
+              accept='image/*'
+              multiple
+            />
+            <label
+              htmlFor='case-study-user-persona'
+              className={styles.userpersona__label}
+            >
+              <div className={styles.view__button}>
+                <div className={styles.upload__icon}>
+                  <PlusIcon />
+                </div>
+                <p className={styles.view__text}>Add User Persona</p>
+              </div>
+            </label>
+            {UserPersonas.length > 0 && (
+              <div
+                className={`${styles.upload} ${styles.userpersona__uploads}`}
+              >
+                {UserPersonas.map((UserPersona, i) => {
+                  return (
+                    <div
+                      key={UserPersona.lastmodified}
+                      className={styles.upload__card}
+                    >
+                      <img
+                        key={UserPersonasURLS[i]}
+                        src={UserPersonasURLS[i]}
+                        alt={UserPersona.name}
+                        className={styles.upload__image}
+                      />
+                      <div className={styles.upload__details}>
+                        <h1 className={styles.upload__name}>
+                          {UserPersona.name}
+                        </h1>
+                        <p className={styles.upload__size}>
+                          {(UserPersona.size / CONVERT_IMAGE_SIZE_MB).toFixed(
+                            2
+                          )}
+                          mb
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          <div className={styles.view__group}>
+            <label htmlFor='case-study-ideation' className={styles.view__title}>
+              Ideation
+            </label>
+            <Tiptap getEditorContent={getEditorContent} />
+          </div>
+          <div className={styles.view__group}>
+            <div className={styles.view__card}>
+              <Input
+                type='file'
+                name='case-study-ideation-image'
+                className={styles.view__file}
+                id='case-study-cover-ideation-image'
+                onChange={uploadIdeationImage}
+                accept='image/*'
+              />
+              <div className={styles.upload}>
+                <label htmlFor='case-study-ideation-image'>
+                  <div className={styles.upload__card}>
+                    {!ideationImages[0] && (
+                      <div className={styles.upload__icon}>
+                        <PlusIcon />
+                      </div>
+                    )}
+                    {ideationImages[0] && (
+                      <img
+                        src={ideationImagesURL[0]}
+                        alt={ideationImages[0].name}
+                        className={styles.upload__image}
+                      />
+                    )}
+                    <div className={styles.upload__details}>
+                      <h1 className={styles.upload__name}>
+                        {ideationImages[0] && ideationImages[0].name}
+                        {!ideationImages[0] && 'Add Image'}
+                      </h1>
+                      <p className={styles.upload__size}>
+                        {ideationImages[0] &&
+                          `${(
+                            ideationImages[0].size / CONVERT_IMAGE_SIZE_MB
+                          ).toFixed(2)}mb`}
+
+                        {!ideationImages[0] && 'Size'}
+                      </p>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className={styles.view__group}>
+            <label htmlFor='case-study-userflow' className={styles.view__title}>
+              Userflow
+            </label>
+            <div className={styles.view__card}>
+              <Input
+                type='file'
+                name='case-study-userflow'
+                className={styles.view__file}
+                id='case-study-userflow'
+                onChange={uploadUserflow}
+                accept='image/*'
+              />
+              <div className={styles.upload}>
+                <label htmlFor='case-study-userflow'>
+                  <div className={styles.upload__card}>
+                    {!userflows[0] && (
+                      <div className={styles.upload__icon}>
+                        <PlusIcon />
+                      </div>
+                    )}
+                    {userflows[0] && (
+                      <img
+                        src={userflowURLS[0]}
+                        alt={userflows[0].name}
+                        className={styles.upload__image}
+                      />
+                    )}
+                    <div className={styles.upload__details}>
+                      <h1 className={styles.upload__name}>
+                        {userflows[0] && userflows[0].name}
+                        {!userflows[0] && 'Case Study Image'}
+                      </h1>
+                      <p className={styles.upload__size}>
+                        {userflows[0] &&
+                          `${(
+                            userflows[0].size / CONVERT_IMAGE_SIZE_MB
+                          ).toFixed(2)}mb`}
+                        {!userflows[0] && 'Size'}
+                      </p>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className={styles.view__group}>
+            <label htmlFor='case-study-colors' className={styles.view__title}>
+              Colours
+            </label>
+            <div className={styles.colors}>
+              {colors.map((color) => (
+                <div key={color.id} className={styles.colors__list}>
+                  <div
+                    className={styles.colors__box}
+                    style={{ background: color.colorCode }}
+                    contentEditable
+                    onBlur={(e) => changeColorHandler(e, color.id)}
+                    onDoubleClick={() => deleteColor(color.id)}
+                    title='Double click to delete'
+                  ></div>
+                  <p className={styles.colors__text}>{color.colorCode}</p>
+                </div>
+              ))}
+              <div className={styles.colors__add}>
+                <div
+                  className={styles.colors__plus}
+                  onClick={generateNewColorHandler}
+                >
+                  <PlusIcon />
+                </div>
+                <p className={styles.colors__text}>Add color</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.view__group}>
+            <label htmlFor='' className={styles.view__title}>
+              Typography
+            </label>
+          </div>
         </form>
-        {/* <Tiptap /> */}
       </DashboardWrapper>
     </div>
   );
 };
-
-// export const getServerSideProps = async function () {
-//   return {
-//     props: {
-//       caseStudyCoverImage: {},
-//       addCaseStudyCoverImage: function (e) {
-//         this.caseStudyCoverImage = e.target.files[0];
-//       },
-//     },
-//   };
-// };
 
 export default AddCaseStudyView;
