45ec05068d0a28e8a6862265a8ab85369f4672ab
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-24 20:16:07 +0100

    Updating homepage responsiveness

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -2,6 +2,7 @@ import styles from './Header.module.scss';
 import NextLink from 'next/link';
 import Logo from '../../logo/Logo';
 import Navigation from '../navigation/Navigation';
+import Button from '../../ui/button/Button';
 
 const Header = function () {
   return (
@@ -14,6 +15,18 @@ const Header = function () {
         </NextLink>
       </div>
       <Navigation />
+      <Button className={styles.header__btn}>
+        <span
+          className={`${styles.header__line} ${styles['header__line--lg']}`}
+        >
+          &nbsp;
+        </span>
+        <span
+          className={`${styles.header__line} ${styles['header__line--sm']}`}
+        >
+          &nbsp;
+        </span>
+      </Button>
     </header>
   );
 };
