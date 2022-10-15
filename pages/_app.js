c90fd4cafa80f5ec67d60681026ff13f79ba836a
Adebesin Cell <adebesintolulope80@gmail.com>
2022-10-15 22:38:20 +0100

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,34 @@ import { initFirebase, app } from '../firebase/firebase';
 import { AuthContextProvider } from '../store/auth-context';
 import { useEffect } from 'react';
 import { getAnalytics, logEvent } from 'firebase/analytics';
+import { useRouter } from 'next/router';
 
 initFirebase();
 
 function MyApp({ Component, pageProps }) {
+  const routers = useRouter();
+
   useEffect(() => {
     if (
       process.env.NODE_ENV === 'production' ||
       process.env.NODE_ENV === 'development'
     ) {
       const analytics = getAnalytics(app);
-      logEvent(analytics, 'screen_view');
+      routers.events.on(
+        'routeChangeComplete',
+        logEvent(analytics, 'page_view', window.location.pathname)
+      );
+
+      logEvent(analytics, 'page_view', window.location.pathname);
+
+      return () => {
+        routers.events.off(
+          'routeChangeComplete',
+          logEvent(analytics, 'page_view', window.location.pathname)
+        );
+      };
     }
-  }, []);
+  }, [routers]);
 
   return (
     <AuthContextProvider>
