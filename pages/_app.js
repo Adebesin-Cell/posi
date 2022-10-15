4dedb110c9ff173d7814e68dd338f68c462e2d1c
Adebesin Cell <adebesintolulope80@gmail.com>
2022-10-15 22:50:19 +0100

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,14 @@
 import '../styles/globals.scss';
-import { initFirebase, app } from '../firebase/firebase';
+import { initFirebase } from '../firebase/firebase';
 import { AuthContextProvider } from '../store/auth-context';
-import { useEffect } from 'react';
-import { getAnalytics, logEvent } from 'firebase/analytics';
-import { useRouter } from 'next/router';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 initFirebase();
 
 function MyApp({ Component, pageProps }) {
-  const routers = useRouter();
-
-  useEffect(() => {
-    if (
-      process.env.NODE_ENV === 'production' ||
-      process.env.NODE_ENV === 'development'
-    ) {
-      const analytics = getAnalytics(app);
-      routers.events.on(
-        'routeChangeComplete',
-        logEvent(analytics, 'page_view', window.location.pathname)
-      );
-
-      logEvent(analytics, 'page_view', window.location.pathname);
-
-      return () => {
-        routers.events.off(
-          'routeChangeComplete',
-          logEvent(analytics, 'page_view', window.location.pathname)
-        );
-      };
-    }
-  }, [routers]);
-
   return (
     <AuthContextProvider>
+      <GoogleAnalytics gaMeasurementId='G-W9CFEC023Q' trackPageViews />
       <Component {...pageProps} />
     </AuthContextProvider>
   );
